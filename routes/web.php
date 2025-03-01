be9560aff69a1f965727dc61315647830594aed7
Béthel <carkbethoun@gmail.com>
2025-03-02 00:16:19 +0100

    Otaku website initialization

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.accueil');
-});
+})->name('accueil');
+
+Route::get('/activite', function () {
+    return view('pages.activite');
+})->name('activite');
